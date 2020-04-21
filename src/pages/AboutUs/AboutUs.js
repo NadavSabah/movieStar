da5dedeb725af907f14a4f1370497f343cdc6417
nadav <nadav1sabah@gmail.com>
2020-04-21 21:47:14 +0300

    before_deploy

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -5,7 +5,7 @@ const AboutUs = () => {
     return (
         <div className="aboutus_container">
             <h1>AboutUs</h1>
-            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            <div className="aboutus_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
